69d65a3edbe8391bcb5034aa996c8d773d6aa184
DHJJJS <lowknjua@gmail.com>
2025-02-12 14:01:40 +0900

    20250212 2시

---
+++ Coding_Dojang/Question09-13.py
@@ -0,0 +1,18 @@
+# 09
+
+a = int(input("a의 값을 입력하세요 : "))
+b = float(input("b의 값을 입력하세요 : "))  
+print(f'a와 b의 합은 {a + b}')
+print(f'a와 b의 평균값 {(a + b) / 2}')
+
+# 10
+
+makit='Sieun Woojin!'
+result = makit
+
+print(result)
+result = 
+
+print(result)
+result = 
+print(result)
